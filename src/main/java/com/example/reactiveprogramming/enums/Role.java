95bd4d6a408cf84eeee9dff6e5f5b0d33d77b84a
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-09-04 15:42:14 +0545

    feat:dto setup for reactive programming

---
+++ src/main/java/com/example/reactiveprogramming/enums/Role.java
@@ -0,0 +1,8 @@
+package com.example.reactiveprogramming.enums;
+
+public enum Role {
+    ADMIN,
+    USER,
+    GUEST
+
+}
---
+++ src/main/java/com/example/reactiveprogramming/service/RecativeServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.reactiveprogramming.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecativeServiceImpl implements Reactive {
+
+    @Override
+    public void saveDeatils() {
+   
+ 
+    }
+
+}
