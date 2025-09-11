b9afb67cac386dfb5abcffae08ddc4df5eb944af
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-09-11 22:00:08 +0545

    note losely coupling

--- src/main/java/com/example/reactiveprogramming/dto/Reactivedto.java
+++ src/main/java/com/example/reactiveprogramming/dto/Reactivedto.java
@@ -1,5 +1,16 @@
 package com.example.reactiveprogramming.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class Reactivedto {
-    
+
+    private String id;
+      
 }
