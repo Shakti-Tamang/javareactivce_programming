f0233a65ed3afeef8b1363b1881cf860e555c31d
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-09-14 12:47:12 +0545

    feat:raetive notes

--- src/main/java/com/example/reactiveprogramming/apiresponse/ApiResponse.java
+++ src/main/java/com/example/reactiveprogramming/apiresponse/ApiResponse.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ApiResponse<T> {
-     @JsonInclude(JsonInclude.Include.NON_NULL)
+   @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("message")
     private String messsage;
 
